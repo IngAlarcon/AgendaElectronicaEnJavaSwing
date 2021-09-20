0607717e4b8d8763efcdd7dd7c3bbd419ca3e4d4
Chango Alarcon <ing.alarconwalter@gmail.com>
2021-09-20 02:01:33 -0300

    Agregando todos mis archivos de mi agenda electronica

---
+++ src/Logica/main.java
@@ -0,0 +1,21 @@
+
+package Logica;
+
+import IGU.InterfazGraficaAgenda;
+
+
+public class main {
+    
+
+    public static void main(String[] args) {
+        
+        InterfazGraficaAgenda registroAgenda = new InterfazGraficaAgenda();// Creo mi ventana 
+        
+        registroAgenda.setVisible(true);// Hago visible mi ventana
+        
+        registroAgenda.setLocationRelativeTo(null);// Centro mi ventana 
+       
+    }
+    
+}
+ 
